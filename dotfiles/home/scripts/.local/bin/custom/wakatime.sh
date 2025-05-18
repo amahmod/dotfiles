ab5a816c0418cafb3ca77e4e097a7f57f970e03b
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2025-05-18 11:54:35 +0600

    remove waktime key

--- dotfiles/home/scripts/.local/bin/custom/wakatime.sh
+++ dotfiles/home/scripts/.local/bin/custom/wakatime.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-TOKEN="waka_38026902-95ea-42c9-9452-81699cfb6d4a"
+TOKEN=""
 
 wakatime_today="$(curl -sf --header "Authorization: Basic $(echo "$TOKEN" | base64)" https://wakatime.com/api/v1/users/current/status_bar/today | jq -r '.data.grand_total.text')"
 
