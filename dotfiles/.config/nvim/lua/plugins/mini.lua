d8e0469f7be858575293b8c5420cb030c54d6e15
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2025-05-30 17:12:05 +0600

    feat(nvim): add mini plugins

---
+++ dotfiles/.config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,10 @@
+return {
+    'echasnovski/mini.nvim',
+    version = '*',
+    config = function()
+        require('mini.ai').setup()
+        require('mini.align').setup()
+        require('mini.pairs').setup()
+        require('mini.icons').setup()
+    end,
+}
