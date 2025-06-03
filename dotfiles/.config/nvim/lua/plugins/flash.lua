c100ca320182c0f1b66ef8f6aad422a7a1c7cb00
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2025-06-03 22:08:22 +0600

    nvim: flash `s` key conflicts with mini.surround keymaps

--- dotfiles/.config/nvim/lua/plugins/flash.lua
+++ dotfiles/.config/nvim/lua/plugins/flash.lua
@@ -5,6 +5,6 @@ return {
     opts = {},
     -- stylua: ignore
     keys = {
-        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+        { "z", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
     },
 }
