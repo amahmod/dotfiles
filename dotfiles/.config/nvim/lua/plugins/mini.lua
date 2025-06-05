8e2b0b2f5af181bf543ecd2609021d202734bf6c
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2025-06-05 13:11:13 +0600

    nvim: update mini.surround mappings

--- dotfiles/.config/nvim/lua/plugins/mini.lua
+++ dotfiles/.config/nvim/lua/plugins/mini.lua
@@ -6,11 +6,21 @@ return {
         require('mini.align').setup()
         require('mini.pairs').setup()
         require('mini.icons').setup()
-        require('mini.surround').setup()
+        require('mini.surround').setup {
+            mappings = {
+                add = 'Sa',
+                delete = 'Sd',
+                find = 'Sf',
+                find_left = 'SF',
+                highlight = 'Sh',
+                replace = 'Sr',
+                update_n_lines = 'Sn',
+            },
+        }
         require('mini.animate').setup()
         require('mini.splitjoin').setup {
             mappings = {
-                toggle = 'gS',
+                toggle = 'gt',
                 split = 'gs',
                 join = 'gj',
             },
