55ef8e7b5c09be48b489881c17775b3768c3a5f5
Mohammad Apel Mahmod <dev.amahmod@gmail.com>
2025-10-18 14:24:59 +0600

    nvim: use opencode for AI text completion and remove avante and codecompanion

--- dotfiles/.config/nvim/lua/plugins/ai/init.lua
+++ dotfiles/.config/nvim/lua/plugins/ai/init.lua
@@ -1,5 +1,4 @@
 return {
-    require 'plugins.ai.copilot',
-    -- require 'plugins.ai_completion.codecompanion',
-    require 'plugins.ai.avante',
+    require 'plugins.ai.supermaven',
+    require 'plugins.ai.opencode',
 }
